a9d43c54dbfcb3e8c7132dcf51352229fdaecbd3
Santino Lujan <Santinoaugust@gmail.com>
2023-02-05 19:26:30 -0300

    Agregando la validacion en JavaSccript de comas y mayusculas

--- challengue.js
+++ challengue.js
@@ -1,42 +1,37 @@
 const textarea = document.querySelector(".texto-ingresado")
 const mensaje = document.querySelector(".mensaje")
 const copia = document.querySelector(".copiar")
-const validar = false
+var validar = false
 
 function validacion(Mensajevalidado){
-    let letrasmayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    let letrasconacento = ["á","é","í","ó","ú"]
-    for(let i = 0; i < letrasmayusculas.length; i++){
-        if(Mensajevalidado.includes(letrasmayusculas[i])){
+    
+    var letrasmayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var letrasconacento = ["á","é","í","ó","ú"]
+    for(var u = 0; u < letrasmayusculas.length; u = u + 1){
+        if(Mensajevalidado.includes(letrasmayusculas[u])){
             alert("Ingrese solo letras minusculas y sin acento")
-            validar = false
             break
-        } else{
-            validar = true
+        } 
         }
-    }
-    for(let i = 0; i < letrasconacento.length; i ++){
-        if(Mensajevalidado.includes(letrasconacento[i])){
+    for(var a = 0; a < letrasconacento.length; a = a +1){
+        if(Mensajevalidado.includes(letrasconacento[a])){
             alert("Ingrese solo letras minusculas y sin acento")
-            validar = false
             break
-        } else{
-            validar = true
-        }
+        } 
 
     }
- 
+    if(u == 26 && a == 5){
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar
 }
 
-function valida(){
-    let textoescrito = document.querySelector(".texto-ingresado")
-    let validador = textoescrito.match(/^[a-z]*$/)
-    if(!validador || validador === 0) {
-        alert("Solo son permitidas letras minúsculas y sin acentos")
-        location.reload();
-        return true;
-    }
-}
+
+
+
+
 
 function encriptar(MEncriptado){
   
@@ -53,13 +48,15 @@ function encriptar(MEncriptado){
 }
 
 function btnencriptar(){
+    if(validacion(textarea.value) == true){
     const textEncriptado = encriptar(textarea.value)
     mensaje.value = textEncriptado
     textarea.value = "";
     mensaje.style.backgroundImage = "none"
     copia.textContent =  "Copiar"
-    
+    } 
 }
+
 
 
 function desencriptar(Mdesencriptado){
