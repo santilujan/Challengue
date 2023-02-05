e5991ebfae824fb937cf791b433e01cff598f43c
Santino Lujan <Santinoaugust@gmail.com>
2023-02-05 00:38:47 -0300

    Ultimas modificaciones al CSS agregando las imagenes y link de git y linkedin

--- challengue.js
+++ challengue.js
@@ -1,26 +1,45 @@
 const textarea = document.querySelector(".texto-ingresado")
 const mensaje = document.querySelector(".mensaje")
 const copia = document.querySelector(".copiar")
+const validar = false
 
 function validacion(Mensajevalidado){
     let letrasmayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     let letrasconacento = ["á","é","í","ó","ú"]
-    Mensajevalidado = Mensajevalidado
     for(let i = 0; i < letrasmayusculas.length; i++){
         if(Mensajevalidado.includes(letrasmayusculas[i])){
             alert("Ingrese solo letras minusculas y sin acento")
+            validar = false
             break
+        } else{
+            validar = true
         }
+    }
+    for(let i = 0; i < letrasconacento.length; i ++){
         if(Mensajevalidado.includes(letrasconacento[i])){
             alert("Ingrese solo letras minusculas y sin acento")
+            validar = false
             break
+        } else{
+            validar = true
         }
-        
+
+    }
+ 
+}
+
+function valida(){
+    let textoescrito = document.querySelector(".texto-ingresado")
+    let validador = textoescrito.match(/^[a-z]*$/)
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
     }
 }
 
-
 function encriptar(MEncriptado){
+  
     let keys = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
     MEncriptado = MEncriptado
 
@@ -30,12 +49,10 @@ function encriptar(MEncriptado){
         }
     }
     return MEncriptado
+
 }
 
 function btnencriptar(){
-
-     
-
     const textEncriptado = encriptar(textarea.value)
     mensaje.value = textEncriptado
     textarea.value = "";
